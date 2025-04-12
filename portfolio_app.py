9e0729ca0f7035e517e602fb79dfe48af22675e1
Michael04-colab <154000418+Michael04-colab@users.noreply.github.com>
2025-04-12 06:36:44 +0000

    Add files via upload

--- portfolio_app.py
+++ portfolio_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/16SHdYp-Vhv0lTByo4u4L4WCPsn5cKrgt
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
